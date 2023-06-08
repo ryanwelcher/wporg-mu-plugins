d7533a7fda4ab667d88cb25a490b3f309a030bda
Ryan Welcher <me@ryanwelcher.com>
2023-06-08 15:22:15 +0300

    Update the language-support plugin use the scripts build process.

---
+++ mu-plugins/blocks/src/language-suggest/edit.js
@@ -0,0 +1,25 @@
+/**
+ * WordPress dependencies
+ */
+import { useBlockProps } from '@wordpress/block-editor';
+
+/**
+ * Internal dependencies
+ */
+import './postcss/style.pcss';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @return {WPElement} Element to render.
+ */
+export default function Edit() {
+	return (
+		<div { ...useBlockProps() }>
+			<div>
+				WordPress is also available in <a href="http://wordpress.org">language</a> (if applicable).
+			</div>
+		</div>
+	);
+}
