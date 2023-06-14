61ab8349c0301fdfd407713349b4f3d0b52779d3
Ryan Welcher <me@ryanwelcher.com>
2023-06-14 16:13:33 -0400

    Commit temp directory for now.

--- mu-plugins/blocks/src/site-breadcrumbs/src/index.js
+++ mu-plugins/blocks/src/site-breadcrumbs/src/index.js
@@ -9,8 +9,18 @@ import { useBlockProps } from '@wordpress/block-editor';
  */
 import metadata from './block.json';
 
+/**
+ * Add a fake preview for the editor.
+ */
 function Edit() {
-	return <div { ...useBlockProps() }>Table of contents</div>;
+	return (
+		<div { ...useBlockProps() }>
+			<span>
+				<a href="#">Home</a> { /* eslint-disable-line jsx-a11y/anchor-is-valid */ }
+			</span>
+			<span className="is-current-page">Current Page</span>
+		</div>
+	);
 }
 
 registerBlockType( metadata.name, {
